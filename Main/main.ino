7344f161c2e26c736ae492dea6a8aa9801d8eecf
PresentMonkey <PresentMonkey@users.noreply.github.com>
2020-01-02 20:56:27 -0500

    Added Welcome Screen

--- Main/main.ino
+++ Main/main.ino
@@ -2,6 +2,8 @@
 #include <MCUFRIEND_kbv.h>   // Hardware-specific library
 MCUFRIEND_kbv tft;
 
+#include <FreeDefaultFonts.h>
+
 #define BLACK   0x0000
 #define RED     0xF800
 #define GREEN   0x07E0
@@ -17,14 +19,26 @@ void setup(void)
     tft.setRotation(0);
     delay(50);
     tft.fillScreen(BLACK);
+    startupTEXT();
 }
 
 void loop(void)
 {
-    tft.setTextColor(RED, GREY);
-    tft.setTextSize(2);
-    tft.setCursor(0, 220);
-    tft.print("7x5 can overwrite");
-    delay(1000);
 
+  
+
+ 
+
+}
+void startupTEXT(void){
+    tft.setTextColor(WHITE);
+    tft.setTextSize(4);
+    tft.setCursor(35 , 90);
+    tft.print("Welcome");
+    tft.setTextSize(2);
+    tft.setCursor(40, 150);
+    tft.print("Developed by:");
+    tft.setTextSize(1);
+    tft.setCursor(50, 200);
+    tft.print(" Engineering Students");
 }
