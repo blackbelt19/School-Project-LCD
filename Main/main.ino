25723e8ad8cb7b890d66ccb30b7c9a66c76e0788
PresentMonkey <33790007+PresentMonkey@users.noreply.github.com>
2020-01-03 10:26:27 -0500

    Add Comments

--- Main/main.ino
+++ Main/main.ino
@@ -1,10 +1,15 @@
+/*
+Developed by Patrick Gawel
+All right reserverd 2020
+
+*/
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <MCUFRIEND_kbv.h>   // Hardware-specific library
-MCUFRIEND_kbv tft;
+MCUFRIEND_kbv tft;     //Initlize screen      
 
 #include <FreeDefaultFonts.h>
 
-#define BLACK   0x0000
+#define BLACK   0x0000   //Define Basic Colors
 #define RED     0xF800
 #define GREEN   0x07E0
 #define WHITE   0xFFFF
@@ -12,25 +17,16 @@ MCUFRIEND_kbv tft;
 
 void setup(void)
 {
-    Serial.begin(9600);
-    uint16_t ID = tft.readID();
-    if (ID == 0xD3) ID = 0x9481;
+    Serial.begin(9600);  //Start Serial
+    uint16_t ID = tft.readID();  //Get ID of scren
+    if (ID == 0xD3) ID = 0x9481;  //Start Screen at ID location
     tft.begin(ID);
-    tft.setRotation(0);
+    tft.setRotation(0);  //Set photomode (Landscape/portrait)
     delay(50);
-    tft.fillScreen(BLACK);
-    startupTEXT();
+    tft.fillScreen(BLACK);  //Make screen Black
+    startupSCREEN();  //Loading screen
 
-    for(int i=0; i<300; i=i+2){
-      if(i==0){
-        delay(1000);
-      }
-      if(i==200){
-        delay(1500);
-      }
-      tft.fillRect(0,300, i, 10, WHITE);
-      delay(50);
-    }
+
 }
 
 void loop(void)
@@ -41,8 +37,8 @@ void loop(void)
  
 
 }
-void startupTEXT(void){
-    tft.setTextColor(WHITE);
+void startupSCREEN(void){
+    tft.setTextColor(WHITE);  //Write the Text
     tft.setTextSize(4);
     tft.setCursor(35 , 90);
     tft.print("Welcome");
@@ -51,5 +47,14 @@ void startupTEXT(void){
     tft.print("Developed by:");
     tft.setTextSize(1);
     tft.setCursor(50, 200);
-    tft.print(" The Superior Major");  
+
+    for(int i=0; i<300; i=i+2){   //Loading Bar
+      if(i==0){                   //Wait for clear screen to finish
+        delay(1000);            
+      }
+      tft.fillRect(0,300, i, 10, WHITE); //Draw Rectange
+      delay(50);
+    }
+    delay(1000);
+    tft.fillScreen(BLACK);   //Clear Screen
 }
